64f29ce51d239624fdd2fed9ba7b9b6f38c943b3
Alutta <Aluttamatambo5@gmail.com>
2024-08-30 10:24:53 +0200

    added const reg ex

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
-
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+     
+    //regular expression
+    const regex =/^pet_[a-zA-Z0-9]+$/;
+    
 
             document.getElementById('result').innerText = result;
 }
